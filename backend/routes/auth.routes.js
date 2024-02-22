0e3e554d1b5eabc9127b8de21a1d1cbc7c286088
akasharyanofficial7 <akasharyangupta7@gmail.com>
2024-02-22 14:04:58 +0530

    create the signin API

--- backend/routes/auth.routes.js
+++ backend/routes/auth.routes.js
@@ -3,5 +3,5 @@ const router = express.Router();
 import { signup } from "../controllers/auth.controllers.js";
 
 router.post("/signup", signup);
-
+router.post("/signin", signup);
 export default router;
