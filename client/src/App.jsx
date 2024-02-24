0b467c4717a3da2a6782b521c26e3ac5d362d5b1
akasharyanofficial7 <akasharyangupta7@gmail.com>
2024-02-24 18:04:41 +0530

    private the dashboard

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -9,6 +9,7 @@ import Projects from "./pages/Projects";
 import SignUp from "./pages/SignUp";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
+import PrivateRoute from "./components/PrivateRoute";
 const App = () => {
   return (
     <BrowserRouter>
@@ -18,7 +19,9 @@ const App = () => {
         <Route path="/about" element={<About />} />
         <Route path="/sign-in" element={<SignIn />} />
         <Route path="/sign-up" element={<SignUp />} />
-        <Route path="/dashboard/tab=profile" element={<Dashboard />} />
+        <Route element={<PrivateRoute />}>
+          <Route path="/dashboard" element={<Dashboard />} />
+        </Route>
         <Route path="/projects" element={<Projects />} />
       </Routes>
 
