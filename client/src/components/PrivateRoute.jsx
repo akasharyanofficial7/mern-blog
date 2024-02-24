fc8bcd83a1e395aa1296b575b8966ed8c31930db
akasharyanofficial7 <akasharyangupta7@gmail.com>
2024-02-24 18:08:48 +0530

    add navigate to sign in page

--- client/src/components/PrivateRoute.jsx
+++ client/src/components/PrivateRoute.jsx
@@ -3,7 +3,7 @@ import { Outlet, Navigate } from "react-router-dom";
 import { useSelector } from "react-redux";
 const PrivateRoute = () => {
   const { currentUser } = useSelector((state) => state.user);
-  return currentUser ? <Outlet /> : null;
+  return currentUser ? <Outlet /> : <Navigate to="/sign-in" />;
 };
 
 export default PrivateRoute;
