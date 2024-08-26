dc79212c00b6fb78cf54c61327f0ec50b8f87fdc
Akash Gupta <114802559+akasharyanofficial7@users.noreply.github.com>
2024-08-27 03:01:18 +0530

    Update vite.config.js

--- client/vite.config.js
+++ client/vite.config.js
@@ -4,8 +4,7 @@ import react from "@vitejs/plugin-react-swc";
 export default defineConfig({
   server: {
     proxy: {
-      "/api": {
-        target: "http://localhost:3000",
+      "/api": "https://mern-blog-backend-1bum.onrender.com",
         secure: false,
       },
     },
